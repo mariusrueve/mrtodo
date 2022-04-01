ec1bee2b5f14c8faac1551e58fec0ad6ed206768
Marius R <44000950+mariusrueve@users.noreply.github.com>
2022-04-01 11:07:07 +0200

    Add functions for basic management of to-dos

--- mrtodo/mrtodo.py
+++ mrtodo/mrtodo.py
@@ -1,9 +1,74 @@
 """This module provides the mrtodo model-controller."""
 # mrtodo/mrtodo.py
 
-from typing import Any, Dict, NamedTuple
+from pathlib import Path
+from typing import Any, Dict, List, NamedTuple
+from mrtodo import DB_READ_ERROR, ID_ERROR
+from mrtodo.database import DatabaseHandler
 
 
-class CurrentTod(NamedTuple):
+class CurrentTodo(NamedTuple):
     todo: Dict[str, Any]
     error: int
+
+
+class Todoer:
+    def __init__(self, db_path: Path) -> None:
+        self._db_handler = DatabaseHandler(db_path)  # Initialize object of class DatabaseHandler() with given path
+
+    def add(self, description: List[str], priority: int = 2) -> CurrentTodo:
+        """Add a new to-do to the database.
+
+        Args:
+            description: A list of strings, which typer builds from the words entered at the command line
+            priority: An integer value representing the to-do's priority. Default = 2, indicating a medium priority.
+        """
+        description_text = " ".join(description)
+        if not description_text.endswith("."):
+            description_text += "."
+        todo = {
+            "Description": description_text,
+            "Priority": priority,
+            "Done": False
+        }
+        read = self._db_handler.read_todos()
+        if read.error == DB_READ_ERROR:
+            return CurrentTodo(todo, read.error)
+        read.todo_list.append(todo)
+        write = self._db_handler.write_todos(read.todo_list)
+        return CurrentTodo(todo, write.error)
+
+    def get_todo_list(self) -> List[Dict[str, Any]]:
+        """Return the current to-do list."""
+        read = self._db_handler.read_todos()
+        return read.todo_list
+
+    def set_done(self, todo_id: int) -> CurrentTodo:
+        """Set a to-do as done."""
+        read = self._db_handler.read_todos()
+        if read.error:
+            return CurrentTodo({}, read.error)
+        try:
+            todo = read.todo_list[todo_id - 1]
+        except IndexError:
+            return CurrentTodo({}, ID_ERROR)
+        todo["Done"] = True
+        write = self._db_handler.write_todos(read.todo_list)
+        return CurrentTodo(todo, write.error)
+
+    def remove(self, todo_id: int) -> CurrentTodo:
+        """Remove a to-do from the database using its id or index."""
+        read = self._db_handler.read_todos()
+        if read.error:
+            return CurrentTodo({}, read.error)
+        try:
+            todo = read.todo_list.pop(todo_id - 1)
+        except IndexError:
+            return CurrentTodo({}, ID_ERROR)
+        write = self._db_handler.write_todos(read.todo_list)
+        return CurrentTodo(todo, write.error)
+
+    def remove_all(self) -> CurrentTodo:
+        """Remove all to-dos from the database."""
+        write = self._db_handler.write_todos([])
+        return CurrentTodo({}, write.error)
